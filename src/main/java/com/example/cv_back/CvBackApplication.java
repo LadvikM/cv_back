6c329229574ed5ad22210a7eef499adc70582711
LadvikM <Ladvik@gmail.com>
2023-03-10 11:33:40 +0200

    Initial Commit

---
+++ src/main/java/com/example/cv_back/CvBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cv_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cv_back/CvBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cv_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
